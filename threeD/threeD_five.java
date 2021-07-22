6075318d2df1a6b0e1ad13140f39d8accee0e154
HsiaoFeng <vectorst@foxmail.com>
2021-07-22 11:00:43 +0800

    修改程序 bug。

--- threeD/threeD_five.java
+++ threeD/threeD_five.java
@@ -11,10 +11,14 @@ public class threeD_five{
         int bet_4;            //下注号码
         int bet_5;            //下注号码
         int winNum;         //开奖号码
+
         System.out.print("共进行期数: ");
         int allTerm = scan.nextInt();
+
         long startTime = System.currentTimeMillis() ;
+
         while(term < allTerm){
+            //进入开奖循环
             term++;
             bet_1 = creatNum();            //下注号码
             bet_2 = creatNum();            //下注号码
@@ -30,12 +34,15 @@ public class threeD_five{
             }
         }
         int income = term*(-10) + prize;
+
         long endTime = System.currentTimeMillis() ;
+
         System.out.println("共进行 "+term+" 期\n花费 "+term*10+" 元\n赢得 "+prize+" 元\n总收益 "+income+" 元\n计算用时 "+ (endTime - startTime) +"ms");
         scan.close();
     }
 
     public static int creatNum(){
+        //机选号码
         Random num = new Random();
         int numH = num.nextInt(10);
         int numT = num.nextInt(10);
@@ -44,25 +51,17 @@ public class threeD_five{
     }
 
     public static int[] betNum(int[] num){
+        //去重机制
         boolean mark = false;
-        for(int a:num){
-            for(int b:num){
-                if(a == b) mark = true;
-                break;
+        for(int i = 0; i < num.length; i++){
+            for(int j = i + 1; j < num.length; j++){
+                if(num[i] == num[j]){
+                    mark = true;
+                    num[j] = creatNum(); 
+                }
             }
         }
-        if(mark){
-            int bet_1 = creatNum();            //下注号码
-            int bet_2 = creatNum();            //下注号码
-            int bet_3 = creatNum();            //下注号码
-            int bet_4 = creatNum();            //下注号码
-            int bet_5 = creatNum();            //下注号码
-            int newNum[] = {bet_1, bet_2, bet_3, bet_4, bet_5};
-            return newNum;
-        } else {
-            int newNum[] = num;
-            return newNum;
-        }
-        
+        if(mark) betNum(num);
+        return num;
     }
 }
